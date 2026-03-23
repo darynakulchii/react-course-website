0bb5d0933491f9bfe53f18c9bddef36e07920386
Daryna <darynakkulchii@gmail.com>
2026-03-23 14:52:54 +0200

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,22 @@
+import './App.css'
+import {Route, Routes} from "react-router-dom";
+import Home from "./pages/Home.jsx";
+import Auth from "./pages/Auth.jsx";
+import Checkout from "./pages/Checkout.jsx";
+import Navbar from "./components/Navbar.jsx";
+
+function App() {
+
+  return (
+    <div className="app">
+        <Navbar />
+        <Routes>
+            <Route path="/" element={<Home />} />
+            <Route path="/auth" element={<Auth />} />
+            <Route path ="/checkout" element={<Checkout />} />
+        </Routes>
+    </div>
+  )
+}
+
+export default App
