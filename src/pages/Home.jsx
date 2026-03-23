e6f80e94949953301aab84bba5e866a0b2c171fa
Daryna <darynakkulchii@gmail.com>
2026-03-23 14:50:40 +0200

    first commit

---
+++ src/pages/Home.jsx
@@ -0,0 +1,21 @@
+import {getProductById, getProducts} from "../data/products.js";
+import {Link} from "react-router-dom";
+import ProductCard from "../components/ProductCard.jsx";
+
+export default function Home(){
+    const products = getProducts();
+    return <div className="page">
+        <div className="home-hero">
+            <h1 className="home-title">Welcome To ShopHub</h1>
+            <p className="home-subtitle">Discover amazing products at great prices</p>
+        </div>
+        <div className="container">
+            <h2 className="page-title">Our Products</h2>
+            <div className="product-grid">
+                {products.map((product) => (
+                    <ProductCard product={product} />
+                ))}
+            </div>
+        </div>
+    </div>
+}
